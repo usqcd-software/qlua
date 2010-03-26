f6dfb96fc85506ce5e65b49613f4db7585332ad7
avp <avp@65d2813f-f11f-0410-b17b-8647a28db151>
2010-03-26 23:38:33 +0000

    Preparing for new handling of extras.
    git-svn-id: file:///Users/avp/tmp/svn.20121212/LHPC/qlua/branches/qlua-ml@1461 65d2813f-f11f-0410-b17b-8647a28db151

--- sources.old/qmdwf.h
+++
@@ -1,7 +0,0 @@
-#ifndef MARK_78C74921_4CB6_467F_9F78_CC914C9622B7
-#define MARK_78C74921_4CB6_467F_9F78_CC914C9622B7
-
-int init_mdwf(lua_State *L);
-int fini_mdwf(lua_State *L);
-
-#endif /* !defined(MARK_78C74921_4CB6_467F_9F78_CC914C9622B7) */
