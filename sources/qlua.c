8ce1a839675530caf832177113e05e5fd4d8d590
avp <avp@65d2813f-f11f-0410-b17b-8647a28db151>
2012-06-06 15:34:57 +0000

    Version 0.31.00-rc1 frozen.
    git-svn-id: file:///Users/avp/tmp/svn.20121212/LHPC/qlua/releases/qlua-0.31.00-rc1@2513 65d2813f-f11f-0410-b17b-8647a28db151

--- sources/qlua.c
+++ sources/qlua.c
@@ -64,7 +64,7 @@ static struct {
     char *value;
 } versions[] = {
         /* XXX qlua version string */
-    {"qlua",  "QLUA version 0.31.00-wk $Id$"},
+    {"qlua",  "QLUA version 0.31.00-rc1 $Id$"},
     {"lua",    LUA_VERSION },
     {"qdp",    QDP_VERSION },
 #ifdef HAS_AFF
